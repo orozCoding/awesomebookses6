871b1fced8fdd893af769b16eed4ab4b05480b4e
orozcoding <orozcoding@gmail.com>
2022-01-10 05:04:42 -0400

    add classes and functions modules

---
+++ modules/classes.js
@@ -0,0 +1,81 @@
+import { main, booksList, bookListSection, sectionTitle } from '../index.js';
+
+export class BookList {
+  constructor() {
+    this.books = [];
+  }
+
+  checkBooks() {
+    if (localStorage.getItem('books')) {
+      this.books = JSON.parse(localStorage.getItem('books'));
+    }
+  }
+
+  saveBooks() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+}
+
+export const bookList = new BookList();
+
+export class Book {
+  constructor(title, author) {
+    this.id = this.idMaker().next().value;
+    this.title = title;
+    this.author = author;
+  }
+
+  // eslint-disable-next-line
+  * idMaker() {
+    let id;
+    if (localStorage.getItem('bookId')) {
+      id = parseInt(localStorage.getItem('bookId'), 10);
+    } else {
+      id = 0;
+    }
+    while (true) yield (id += 1);
+  }
+
+  addBook() {
+    bookList.books.push(this);
+    localStorage.setItem('bookId', this.id);
+    bookList.saveBooks();
+    this.displayBooks();
+  }
+
+  // eslint-disable-next-line
+  removeBook(id) {
+    bookList.books = bookList.books.filter((book) => book.id !== id);
+    bookList.saveBooks();
+  }
+
+  displayBooks() {
+    booksList.innerHTML = '';
+    if (bookList.books.length === 0) {
+      const emptyMessage = document.createElement('p');
+      emptyMessage.textContent = 'No books found!';
+      booksList.appendChild(emptyMessage);
+    } else {
+      bookList.books.forEach((book) => {
+        const bookHTML = document.createElement('div');
+        bookHTML.className = 'book';
+        bookHTML.innerHTML = `
+          <p><q>${book.title}</q> by ${book.author}</p>`;
+        const removeBtn = document.createElement('button');
+        removeBtn.setAttribute('type', 'button');
+        removeBtn.id = `remove-book-${book.id}`;
+        removeBtn.classList.add('btn', 'btn-remove', 'clickeable');
+        removeBtn.innerHTML = 'Remove';
+        removeBtn.addEventListener('click', () => {
+          this.removeBook(
+            bookList.books.find((item) => item.id === book.id).id,
+          );
+          removeBtn.parentElement.remove();
+        });
+        bookHTML.appendChild(removeBtn);
+        booksList.appendChild(bookHTML);
+      });
+      bookListSection.appendChild(booksList);
+    }
+  }
+}
